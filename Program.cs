911259579e2c08e8ad9d5714f0bd4e5467adf863
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-04-29 14:37:57 +0700

    ok

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReadCSVExample
+{
+    class Country
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Country> countries = new List<Country>();
+            string filePath = "countries.csv";// Thêm địa chỉ  file gốc vào đây để thực hiện đọc và ỉn ra console
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(",");
+                        Country country = new Country()
+                        {
+                            Code = fields[4],
+                            Name = fields[5]
+                        };
+                        countries.Add(country);
+                    }
+                }
+                Console.WriteLine("List of countries:");
+                foreach (var country in countries)
+                {
+                    Console.WriteLine(country.Code + ": " + country.Name);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading the file: " + e.Message);
+            }
+        }
+    }
+}
